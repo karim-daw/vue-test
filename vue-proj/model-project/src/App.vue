0a8ce85425ed8566801ac46db9feaf78f4da7d76
karimdaw1991 <karimdaw91@gmail.com>
2021-11-17 23:41:07 +0100

    add

--- vue-proj/model-project/src/App.vue
+++ vue-proj/model-project/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
@@ -9,6 +11,16 @@ export default {
   data() {
     return {
       title: 'Hey! I\'m Karim :)'
+    }
+  },
+  // this is how we add methods to a compoenent
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      // classList is JS property, add will add a class in JS
+      this.$refs.name.classList.add('active')
+      // focus method will focus() on DOM field
+      this.$refs.name.focus()
     }
   }
 
